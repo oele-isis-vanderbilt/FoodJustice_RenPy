6580fe14674274c63195cb4019b33cd985d3ab3f
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2025-04-15 09:30:09 -0500

    Merge the teacher demo app with the main branch. Closes (#28)
    * Create newfolder.txt
    
    adding folder to store teacher version
    
    * Files for teacher demo version of SciStory
    
    * Delete SciStoryTeacherDemo/newfolder.txt
    
    * Create test.txt
    
    * Update and rename SciStoryTeacherDemo/gui.rpy to SciStoryTeacherDemo/game/gui.rpy
    
    moving files
    
    * Rename SciStoryTeacherDemo/gui.rpyc to SciStoryTeacherDemo/game/gui.rpyc
    
    * Update and rename SciStoryTeacherDemo/options.rpy to SciStoryTeacherDemo/game/options.rpy
    
    * Rename SciStoryTeacherDemo/options.rpyc to SciStoryTeacherDemo/game/options.rpyc
    
    * Update and rename SciStoryTeacherDemo/screens.rpy to SciStoryTeacherDemo/game/screens.rpy
    
    * Rename SciStoryTeacherDemo/screens.rpyc to SciStoryTeacherDemo/game/screens.rpyc
    
    * Update and rename SciStoryTeacherDemo/script.rpy to SciStoryTeacherDemo/game/script.rpy
    
    * Rename SciStoryTeacherDemo/script.rpyc to SciStoryTeacherDemo/game/script.rpyc
    
    * Images for teacher version
    
    * Delete SciStoryTeacherDemo/game/test.txt
    
    * Add files via upload
    
    * Add files via upload
    
    adding the rest of the necessary renpy files
    
    * updated button images
    
    making try again easier to see, giving robot button a background
    
    * tweaking appearance of scroll boxes
    
    making sure different response lengths fit properly in agent comparison screen. currently have placeholder responses listed, but can replace them with the commented parts to get live agent responses
    
    * icon for menu bar
    
    * Adding gamelog
    
    Added so local logging backup works with gamelog.txt if http log fails
    
    * Updated Llama CA link from Yeojin
    
    Updated ca_link in script.rpy to reflect the new llama link
    
    * Try and deploy teacher demo
    
    * Fix compose file name
    
    * Fix environment file path
    
    * Add syncflow screenshare
    
    * FIx environment's location
    
    * added eca_length_check function
    
    adding checking for agent response length, splits response into two different say statements if over 200 characters, to avoid text overflow during dialogue
    
    * Fix thing I broke (sorry!)
    
    adding back the syncflow functions i accidentally removed with the last commit
    
    * Adding Namrata's GPT integration
    
    adding gpt_agent_setup and lines to call gpt agent during CA agent test block
    
    * Update script.rpy to remove logging feature
    
    * New GPT link with https
    
    * Update screens.rpy
    
    removed placeholder text so it should show live agent answers now
    
    * Use correct keyword argument for automatic recording
    
    * Explicitly clone SyncFlowClient
    
    * Change branch reference
    
    ---------
    
    Co-authored-by: Humblebug77 <meganhumburg@gmail.com>

---
+++ SciStoryTeacherDemo/syncflow/syncflow-publisher.js
@@ -0,0 +1,100 @@
+class SyncFlowPublisher{
+    constructor(
+        enabled,
+        enableCamera,
+        enableMicrophone,
+        enableScreenShare,
+    ) {
+        this.enabled = enabled;
+        this.enableCamera = enableCamera;
+        this.enableMicrophone = enableMicrophone;
+        this.enableScreenShare = enableScreenShare;
+        this.room = null;
+        this.encoder = new TextEncoder();
+    }
+
+    static async initialize() {
+        const API_URL = "/syncflow/runtime-settings";
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            return new SyncFlowPublisher(false, false, false, false);
+        } else {
+            const data = await response.json();
+            return new SyncFlowPublisher(
+                data.enabled,
+                data.enableCamera,
+                data.enableMicrophone,
+                data.enableScreenShare
+            );
+        }
+    }  
+
+    async startPublishing(identity, name) {
+        const TOKEN_ENDPOINT = `/syncflow/token?identity=${identity}`;
+        const response = await fetch(
+            TOKEN_ENDPOINT
+        );
+
+        if (response.ok && window.LivekitClient) {
+            const tokenResponse = await response.json();
+
+            const room = new window.LivekitClient.Room({
+                adaptiveStream: true,
+                dynacast: true,
+
+                videoCaptureDefaults: {
+                    resolution: window.LivekitClient.VideoPresets.h720.resolution
+                }
+            });
+
+            await room.connect(tokenResponse.livekitServerUrl, tokenResponse.token);
+            // await room.localParticipant.enableCameraAndMicrophone();   
+            console.log("Publishing screenshare");         
+            const publication = await room.localParticipant.setScreenShareEnabled(
+				true,
+				{
+					contentHint: 'detail',
+					audio: false,
+                    resolution: { width: 1920, height: 1080 },
+					video: { displaySurface: 'window' }
+				},
+				{
+					videoCodec: 'h264',
+					simulcast: true
+				}
+			);
+            console.log("Screenshare enabled");
+            this.room = room;
+        }
+    }
+
+    async logEvent(user, action, view, timestamp, payload) {    
+        if (this.room) {
+            console.log("Publishing data to room");
+            await this.room.localParticipant.publishData(
+                this.encoder.encode(
+                    JSON.stringify({
+                        user: user,
+                        action: action,
+                        view: view,
+                        timestamp: timestamp,
+                        payload: payload
+                    })
+                ),
+                {
+                    reliable: true
+                }
+            );
+            console.log("Published data to room");
+        }
+    }
+}
+
+SyncFlowPublisher.initialize().then((publisher) => {
+    console.log("SyncFlowPublisher initialized");
+    console.log("SyncFlowPublisher enabled: " + publisher.enabled);
+    console.log("SyncFlowPublisher enableCamera: " + publisher.enableCamera);
+    console.log("SyncFlowPublisher enableMicrophone: " + publisher.enableMicrophone);
+    console.log("SyncFlowPublisher enableScreenShare: " + publisher.enableScreenShare);
+    window.syncFlowPublisher = publisher;
+});
